6812349376f7f529e640fe8807f8d31ad43e40e9
izzycode <marcemc@yahoo.com>
2016-11-17 21:06:21 -0500

    Scaffold Playlist and Song model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  resources :songs
+  resources :playlists
+  get 'playlists/index'
+
+  get 'playlists/show'
+
+  get 'playlists/create'
+
+  get 'playlists/delete'
+
+  get 'playlists/update'
+
+  get 'playlists/edit'
+
   root 'welcome#home'
   get 'welcome/home'
 
